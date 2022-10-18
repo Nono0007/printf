f8fcd0056d136b752f686f8766ef047573138ccb
VeniceSandie <sandisiwe2405@gmail.com>
2022-10-18 05:30:44 -0700

    Team_task

--- print_nums.c
+++ print_nums.c
@@ -1,84 +1,45 @@
 #include "main.h"
 
 /**
- * print_int - prints an integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
+ * print_hex - prints a number in hexadecimal base,
+ * in lowercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
- * Return: number of char printed
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-int print_int(va_list l, flags_t *f)
+int print_hex(va_list l, flags_t *f)
 {
-        int n = va_arg(l, int);
-        int res = count_digit(n);
+        unsigned int num = va_arg(l, unsigned int);
+        char *str = convert(num, 16, 1);
+        int count = 0;
 
-        if (f->space == 1 && f->plus == 0 && n >= 0)
-                res += _putchar(' ');
-        if (f->plus == 1 && n >= 0)
-                res += _putchar('+');
-        if (n <= 0)
-                res++;
-        print_number(n);
-        return (res);
+        if (f->hash == 1 && str[0] != '0')
+                count += _puts("0x");
+        count += _puts(str);
+        return (count);
 }
 
 /**
- * print_unsigned - prints an unsigned integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
+ * print_hex_big - prints a number in hexadecimal base,
+ * in uppercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
  * if a flag is passed to _printf
- * Return: number of char printed
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-int print_unsigned(va_list l, flags_t *f)
+int print_hex_big(va_list l, flags_t *f)
 {
-        unsigned int u = va_arg(l, unsigned int);
-        char *str = convert(u, 10, 0);
+        unsigned int num = va_arg(l, unsigned int);
+        char *str = convert(num, 16, 0);
+        int count = 0;
 
-        (void)f;
-        return (_puts(str));
-}
-
-/**
- * print_number - helper function that loops through
- * an integer and prints all its digits
- * @n: integer to be printed
- */
-void print_number(int n)
-{
-        unsigned int n1;
-
-        if (n < 0)
-        {
-                _putchar('-');
-                n1 = -n;
-        }
-        else
-                n1 = n;
-
-        if (n1 / 10)
-                print_number(n1 / 10);
-        _putchar((n1 % 10) + '0');
-}
-
-/**
- * count_digit - returns the number of digits in an integer
- * for _printf
- * @i: integer to evaluate
- * Return: number of digits
- */
-int count_digit(int i)
-{
-        unsigned int d = 0;
-        unsigned int u;
-
-        if (i < 0)
-                u = i * -1;
-        else
-                u = i;
-        while (u != 0)
-        {
-                u /= 10;
-                d++;
-        }
-        return (d);
+        if (f->hash == 1 && str[0] != '0')
+                count += _puts("0X");
+        count += _puts(str);
+        return (count);
 }
